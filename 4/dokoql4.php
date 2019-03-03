18f6a420354b222728c995c2f63a2b113464463f
平石匡 <masahiraishi22@gmail.com>
2019-03-03 22:09:36 +0900

    sql

--- 4/dokoql4.php
+++ 4/dokoql4.php
@@ -29,7 +29,36 @@ SELECT himoku,syukkinngaku
 						else 'henndouhi'
 						end as syubpinobunnrui
 						from kakeibo
-						where syukinngaku > 0
+						where syukinngaku > 0;
 						
 
+select memo, Length(memo) as memonobnagasa from kakeibo
+
+select
+ 	sum(syukinngaku) as goukeisyukiingaku,
+	avg(syukiingaku) as heikinnsyukkingaku,
+	max(syukiingaku) as sannzai,
+	min(syukiingaku) assyougaku,
+FROM kakeibo
+
+select count(*) as syokuhi
+from kakeibo
+where himoku = 'syokuhi';
+
+
+SELECT himoku,AVG(syukinngaku) as himokubetu
+from kakeibo
+group by himoku;
+
+
+select himoku,sum(syukinngaku) as himokubetu
+from kakeibo
+group by himoku
+having sum(syukinngaku) > 0;
+
+
+SELECT hiduke,count(*) from kanri where ziyu = 3 group by hiduke having hiduke;
+
+select syainnmei ,count(*) from kannri where 
 EOF;
+
